57a0abb6f51d23eec4967d3076ca3107d706d459
lenovi <mr.lenovi@gmail.com>
2026-05-12 11:25:54 +0700

    api

--- fe/src/services/api.js
+++ fe/src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000/api';
+export const BASE_URL = 'https://calendar-ny17.onrender.com/api';
 
 export const apiFetch = async (endpoint, options = {}) => {
   const url = `${BASE_URL}${endpoint}`;

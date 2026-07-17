26c1e92eb9c8e8dc3247ee8e06c0fe064640bdad
ADat1304 <datvit2k5@gmail.com>
2026-07-17 17:33:12 +0700

    Dat add 2 thread for notification

--- be/workers/notificationWorker.js
+++ be/workers/notificationWorker.js
@@ -8,7 +8,7 @@ const worker = new Worker('notification-queue', async (job) => {
   if (job.name === 'send-push') {
     const { subscriptionIds, title, content, url, buttons } = job.data;
     console.log(`[Notification Worker] Processing job ${job.id} - sending push to ${subscriptionIds.length} subscriptions`);
-    
+
     try {
       const result = await sendPushNotification(subscriptionIds, title, content, url, buttons);
       return result;
@@ -18,7 +18,8 @@ const worker = new Worker('notification-queue', async (job) => {
     }
   }
 }, {
-  connection
+  connection,
+  concurrency: 5
 });
 
 worker.on('completed', (job) => {

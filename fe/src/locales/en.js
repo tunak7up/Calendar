af7a66dfc30061588431e34ea097ae25d329a3d9
tuna <tunak7up@gmail.com>
2026-08-06 16:44:34 +0700

    test

--- fe/src/locales/en.js
+++ fe/src/locales/en.js
@@ -1,7 +1,7 @@
 export const en = {
   translation: {
     nav: {
-      logo: "Internship Management",
+      logo: "Internship Management test deploy",
       dashboard: "Dashboard",
       schedule: "My Schedule",
       register: "Registration",

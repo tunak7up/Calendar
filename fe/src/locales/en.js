c76f644393626529eb4559781c7a310cab21e980
tuna <tunak7up@gmail.com>
2026-08-07 17:52:08 +0700

    triigger

--- fe/src/locales/en.js
+++ fe/src/locales/en.js
@@ -1,7 +1,7 @@
 export const en = {
   translation: {
     nav: {
-      logo: "Internship Management asdfasdf auto",
+      logo: "Internship Management",
       dashboard: "Dashboard",
       schedule: "My Schedule",
       register: "Registration",
